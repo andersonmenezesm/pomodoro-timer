d48ae038fe7fe3a1a35bb5638f9bf91cf6ac3774
Anderson Menezes <anderson.stifip@gmail.com>
2024-01-30 17:39:44 -0300

    feat: Criação da página de Histórico

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,55 @@
+import { HistoryContainer, HistoryList, Status } from './styles'
+
+export function History() {
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <table>
+          <tr>
+            <th>Tarefa</th>
+            <th>Duração</th>
+            <th>Início</th>
+            <th>Status</th>
+          </tr>
+
+          <tbody>
+            <tr>
+              <td>Tarefa 1</td>
+              <td>30m</td>
+              <td>Há 2 cerca de meses</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa 2</td>
+              <td>30m</td>
+              <td>Há 2 cerca de meses</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa 3</td>
+              <td>30m</td>
+              <td>Há 2 cerca de meses</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa 4</td>
+              <td>30m</td>
+              <td>Há 2 cerca de meses</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
+}
